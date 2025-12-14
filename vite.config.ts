94869f5e08c5dd3b2455aa8c04c040e90d4f6f1d
abeshahsan <abeshahsan2002@gmail.com>
2025-12-14 22:42:27 +0600

    Update base path in Vite configuration to root

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/portfolio/',
+	base: '/',
 	plugins: [react(), tailwindcss(), compression({ algorithm: "brotliCompress" }), compression({ algorithm: "gzip" })],
 	build: {
 		minify: "esbuild",
